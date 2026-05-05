205d414a36e1fa6f08ddc470ac0a30e5604d2b49
Hoang Snowy <nguyenminhhoang.dit12@gmail.com>
2026-05-05 23:29:18 +0700

    fix(mapper): null-forgive Mapster 10 Adapt return

--- src/mapping/Jaina.Mapping.Mapster/MapsterMapper.cs
+++ src/mapping/Jaina.Mapping.Mapster/MapsterMapper.cs
@@ -10,8 +10,8 @@ internal sealed class MapsterMapper : IMapper
     public MapsterMapper(TypeAdapterConfig config) => _config = config;
 
     public TDestination Map<TDestination>(object source)
-        => source.Adapt<TDestination>(_config);
+        => source.Adapt<TDestination>(_config)!;
 
     public TDestination Map<TSource, TDestination>(TSource source)
-        => source.Adapt<TDestination>(_config);
+        => source.Adapt<TDestination>(_config)!;
 }
